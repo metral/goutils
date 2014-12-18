d29de67c7f3de7f09aef91f6abaa7b1740ab03ce
Mike Metral <metral@gmail.com>
2014-12-18 10:09:07 -0800

    Init

---
+++ generic.go
@@ -0,0 +1,22 @@
+
+import (
+	"fmt"
+	"log"
+	"runtime"
+)
+
+// check for errors and panic, if found
+func checkForErrors(err error, stderr string, callerNum int) {
+	if err != nil {
+		pc, fn, line, _ := runtime.Caller(callerNum)
+		msg := ""
+		if stderr != "" {
+			msg = fmt.Sprintf("[Error] in %s[%s:%d] %v: %s",
+				runtime.FuncForPC(pc).Name(), fn, line, err, stderr)
+		} else {
+			msg = fmt.Sprintf("[Error] in %s[%s:%d] %v",
+				runtime.FuncForPC(pc).Name(), fn, line, err)
+		}
+		log.Fatal(msg)
+	}
+}
---
+++ http.go
@@ -0,0 +1,56 @@
+package utils
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+)
+
+func httpGetRequest(url string) []byte {
+	resp, err := http.Get(url)
+	checkForErrors(err)
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	checkForErrors(err)
+
+	return body
+}
+
+func httpPutRequest(urlStr string, data []byte) *http.Response {
+	var req *http.Request
+
+	req, _ = http.NewRequest("PUT", urlStr, bytes.NewBuffer(data))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	checkForErrors(err)
+
+	defer resp.Body.Close()
+
+	return resp
+}
+
+func httpPutRequestRedirect(urlStr string, data string) {
+	var req *http.Request
+	req, _ = http.NewRequest("PUT", urlStr, bytes.NewBufferString(data))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Content-Length", strconv.Itoa(len(data)))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	checkForErrors(err)
+
+	if resp.StatusCode == http.StatusTemporaryRedirect {
+		u, err := resp.Location()
+
+		if err != nil {
+			checkForErrors(err)
+		} else {
+			httpPutRequestRedirect(u.String(), data)
+		}
+		resp.Body.Close()
+	}
+}
